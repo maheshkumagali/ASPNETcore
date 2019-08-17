100ad31763a6cc69f03746f30c9fc69b2ee5275b
Mahesh <maheshkumargali@gmail.com>
2019-08-17 18:19:26 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TagHelpersDemo.Models;
+
+namespace TagHelpersDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            EmployeeList list = new EmployeeList();
+            list.employees = new List<Employee>();
+            list.employees.Add(new Employee { Id = 101, Name = "Mahesh"});
+            list.employees.Add(new Employee { Id = 102, Name = "Ramesh" });
+            return View(list);
+        }
+
+        public IActionResult Details(int id, string foo)
+        {
+            Employee emp = new Employee();
+            emp.Id = 101;
+            emp.Name = "Mahesh";
+            return View(emp);
+        }
+    }
+}
---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpersDemo.Models
+{
+    public class EmployeeList
+    {
+       public List<Employee> employees { get; set; }
+    }
+
+   
+}
